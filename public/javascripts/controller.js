10929d5e7055327a90b7e3bb59c685f5531ef8b1
chamcee89@gmail.com <chamcee89@gmail.com>
2016-06-15 20:46:30 -0300

    factory injection

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -5,29 +5,27 @@
         .module('chamApp')
         .controller('chamController', chamController);
 
-    chamController.$inject = ['$scope'];
+    chamController.$inject = ['$scope', 'todoService'];
 
     /* @ngInject */
-    function chamController($scope) {
+    function chamController($scope, todoService) {
         var vm = this;
         vm.title = 'chamController';
         vm.thingsTodo = '';
-        vm.checkList = [];
+        vm.checkList = todoService.getAllList();
         vm.addNewTodo = function () {
         	vm.checkList.push(vm.thingsTodo);
+            console.log(vm.thingsTodo);
         }
        	vm.deleteTodo = function ( idx ) {
        		vm.checkList.splice(idx, 1);
        		console.log(idx);
        	}
-
         activate();
 
         ////////////////
 
         function activate() {
-        }
-
-        
+        }  
     }
 })();
