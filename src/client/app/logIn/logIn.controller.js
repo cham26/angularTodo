41ca9fa31d2730541055cf9479faf02742bbd026
chamcee89@gmail.com <chamcee89@gmail.com>
2016-07-01 18:30:43 -0300

    fixed additional styles, popover

--- src/client/app/logIn/logIn.controller.js
+++ src/client/app/logIn/logIn.controller.js
@@ -12,12 +12,87 @@
         var vm = this;
         vm.title = 'logInController';
         vm.user = {};
-        vm.user.name = '';
-        vm.user.email = '';
-        vm.user.cash = '';
         vm.validateForm = function() {
             console.log(vm.user);
         }
+        // // datepicker
+        // vm.dt = new Date();
+        // vm.options = {
+        //     customClass: getDayClass,
+        //     minDate: new Date(),
+        //     showWeeks: true
+        // };
+
+        // vm.dateOptions = {
+        //     dateDisabled: disabled,
+        //     formatYear: 'yy',
+        //     maxDate: new Date('+10D'),
+        //     minDate: new Date(),
+        //     startingDay: 1
+        // };
+
+        // // Disable weekend selection
+        // vm.disabled = function(data) {
+        //     vm date = data.date,
+        //     mode = data.mode;
+        //     returns mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        // }
+        // vm.toggleMin = function() {
+        //     vm.inlineOptions.minDate = vm.inlineOptions.minDate ? null : new Date();
+        //     vm.dateOptions.minDate = vm.inlineOptions.minDate;
+        // };
+        // vm.toggleMin();
+
+        // vm.open = function() {
+        //     vm.popup.opened = true;
+        // };
+
+        // vm.setDate = function(year, month, day) {
+        //     vm.dt = new Date(year, month, day);
+        // };
+
+        // vm.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+        // vm.format = vm.formats[0];
+
+        // vm.popup = {
+        //     opened: false
+        // };
+
+        // var tomorrow = new Date();
+        // tommorrow.setDate(tomorrow.getDate() + 1);
+        // var afterTomorrow = new Date();
+        // afterTomorrow.setDate(tomorrow.getDate() + 1);
+        // vm.events = [
+        //     {
+        //         date: tomorrow,
+        //         status: "full"
+        //     },
+        //     {
+        //         date: afteTomorrow,
+        //         status: "partially"
+        //     }
+        // ];
+
+        // vm.getDayClass(data) {
+        //     var date = data.date,
+        //     mode = date.mode;
+        //      if (mode === 'day') {
+        //         cm.dayToCheck == new Date (vm.date).setHours(0,0,0,0);
+
+        //         for (var i = 0; i < vm.events.length; i++) {
+        //             var currentDay = new Date(vm.events[i].date).setHours(0,0,0,0);
+
+        //             if (dayToCheck === currentDay) {
+        //                 return vm.events[i].status;
+        //             }
+        //         }
+        //      }
+
+        //      return '';
+        // }
+
+
+
         activate();
 
         ////////////////
