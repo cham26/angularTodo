279ae3ae920bc0785beead7a8b347c6f729843e7
chamcee89@gmail.com <chamcee89@gmail.com>
2016-07-03 07:59:21 -0300

    ongoing datepicker implementation

--- src/client/app/logIn/component/logInEntry.directive.js
+++ src/client/app/logIn/component/logInEntry.directive.js
@@ -6,3 +6,38 @@ angular
 			templateUrl: "app/logIn/component/logInEntry.component.html"
 		}
 })
+
+    .directive("myDatePicker", function() {
+		return {
+			restrict: "E",
+			scope: {
+				ngModel: "=",
+				dateOptions: "=",
+				opened: "="
+			},
+			link: function($scope, element, attrs) {
+				$scope.open = function(event){
+					console.log("open");
+					event.preventDefault();
+					event.stopPropagation();
+					$scope.opened = true;
+				};
+
+				$scope.clear = function() {
+					$scope.ngModel = null;
+				};
+			},
+			controller: function ($scope, $http) {
+  
+				$scope.formData      = {};
+  				$scope.formData.date = "";
+  				$scope.opened        = false;
+  
+				//Datepicker
+				$scope.dateOptions = {
+				'year-format': "'yy'",
+				'show-weeks' : false
+	};
+});
+		}
+})
