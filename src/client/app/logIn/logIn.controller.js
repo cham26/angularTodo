e944e771c5875a283f970ced87f0f2807d06587e
chamcee89@gmail.com <chamcee89@gmail.com>
2016-07-07 16:22:21 -0300

    min and max date implementation not working

--- src/client/app/logIn/logIn.controller.js
+++ src/client/app/logIn/logIn.controller.js
@@ -17,6 +17,15 @@
         }
         //datepicker
         vm.isDatePickerOpen = false;
+       
+        vm.minDate = new Date();
+        vm.maxDate = new Date();
+        vm.maxDate.setDate(vm.minDate.getDate() +10);
+        console.log(vm.minDate);
+        console.log(vm.maxDate)
+        
+        
+       
         
           
        
